70c5701a329ffd3cf81b88433ef35fbbc60a757d
eftelyaozcan <eftelyaozcan@gmail.com>
2025-10-17 22:55:37 +0300

    Add Week4.2 lab

---
+++ Week4.2/app/src/main/java/msku/ceng/madlab/week4/MainActivity.java
@@ -0,0 +1,64 @@
+package msku.ceng.madlab.week4;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnGo;
+    EditText txtUrl;
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnGo = findViewById(R.id.btnGo);
+        txtUrl = findViewById(R.id.txtUrl);
+        webview = findViewById(R.id.webview);
+
+        webview.setWebViewClient(new WebViewClient());
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webview.loadUrl("https://" + txtUrl.getText());
+            }
+        });
+
+        if(getIntent() != null && getIntent().getData() != null){
+            txtUrl.setText(getIntent().getData().toString());
+            webview.loadUrl(getIntent().getData().toString());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
